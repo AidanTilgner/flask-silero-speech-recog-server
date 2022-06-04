290d32142bd274f02cdc7c74fd818b17c7aa862e
Aidan Tilgner <aidantilgner02@gmail.com>
2022-06-03 18:24:37 -0700

    initial commit
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

---
+++ app.py
@@ -0,0 +1,63 @@
+from time import time
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    url_for,
+    make_response,
+    session,
+)
+from sst import *
+import os
+from flask_cors import CORS
+
+# Initialize the Flask application
+app = Flask(__name__)
+app.secret_key = "A0Zr98j/3yX R~XHH!jmNlLWX/,?cT"
+CORS(app, resources={"/*": {"origins": "*"}})
+
+
+@app.route("/")
+def index():
+    return redirect(url_for("ui"))
+
+
+@app.route("/ui", methods=["GET"])
+def ui(status=302):
+    return render_template("index.html", status=status)
+
+
+# The stt post request will take an audio file and return predicted text
+@app.route("/stt", methods=["POST", "GET"])
+def sst():
+    if request.method == "POST":
+        file = request.files["file"]
+        ext = file.filename.split(".")[-1]
+        filename = "temp." + ext
+        file.save(os.path.join(os.path.dirname(__file__), "tmp/" + filename))
+        text = predictAudio(os.path.join(os.path.dirname(__file__), "tmp/" + filename))
+        os.remove(os.path.join(os.path.dirname(__file__), "tmp/" + filename))
+        # Add text translation to session array\
+        if "translations" in session and session["translations"] is not None:
+            session["translations"].append({"text": text})
+            session["translations"] = session["translations"][-10:]
+        else:
+            session["translations"] = [{"text": text}]
+        return {"text": text}
+
+
+@app.route("/translations", methods=["GET"])
+def translations():
+    # get all translations from session
+    if request.method == "GET":
+        if "translations" in session:
+            return {"translations": session["translations"]}
+        else:
+            return {"translations": []}
+
+
+# start server on port 5000
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
---
+++ sst.py
@@ -0,0 +1,41 @@
+import onnx
+import torch
+import onnxruntime
+from omegaconf import OmegaConf
+import os
+
+language = 'en' # also available 'de', 'es'
+
+# load provided utils
+_, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_stt', language=language)
+(read_batch, split_into_batches,
+ read_audio, prepare_model_input) = utils
+
+ # see available models
+torch.hub.download_url_to_file('https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml', 'models.yml')
+models = OmegaConf.load('models.yml')
+available_languages = list(models.stt_models.keys())
+assert language in available_languages
+
+# load the actual ONNX model
+torch.hub.download_url_to_file(models.stt_models.en.latest.onnx, 'model.onnx', progress=True)
+onnx_model = onnx.load('model.onnx')
+onnx.checker.check_model(onnx_model)
+ort_session = onnxruntime.InferenceSession('model.onnx')
+
+
+def predictAudio(audio_path):
+    """
+    audio_path: path to audio file
+    """
+    # download a single file, any format compatible with TorchAudio
+    test_files = [audio_path]
+    batches = split_into_batches(test_files, batch_size=10)
+    input = prepare_model_input(read_batch(batches[0]))
+
+    # actual onnx inference and decoding
+    onnx_input = input.detach().cpu().numpy()
+    ort_inputs = {"input": onnx_input}
+    ort_outs = ort_session.run(None, ort_inputs)
+    decoded = decoder(torch.Tensor(ort_outs[0])[0])
+    return decoded
